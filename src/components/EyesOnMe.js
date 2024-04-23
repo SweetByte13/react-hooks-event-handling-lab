a50949c28e5c914431df766d033b03e32ea9ad78
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-23 11:30:46 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+
+
+function EyesOnMe() {
+
+function onFocus() {
+    console.log("Good!")
+}
+
+function blur() {
+    console.log("Hey! Eyes on me!")
+}
+
+    return (
+        <div>
+            <button onFocus={onFocus} onBlur={blur}>
+                Eyes on me
+            </button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
